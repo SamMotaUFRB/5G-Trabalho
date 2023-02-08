2b708d69943ffbe21c93311ec360e04549bedc3a
SamMotaUFRB <samuelmota@aluno.ufrb.edu.br>
2023-02-08 14:28:54 -0300

    Utlizando o Tkinter no Python
    Fazendo alguns testes

---
+++ Python Files/Teste com Tkinter.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+app = Tk()
+app.title("APP 5G")
+app.geometry("500x500")
+app.configure(background="#118")
+
+txt1 = Label(app, text = "Bem-vindo a interface", background="#ff0", foreground= "#000")
+txt1.place(x=10,y=10,width=150, height=30)
+
+
+
+
+
+app.mainloop()
