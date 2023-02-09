8d4c2d47918c69ec5aa2f5ceef74d80fe3197eb5
SamMotaUFRB <samuelmota@aluno.ufrb.edu.br>
2023-02-09 11:00:17 -0300

    Escrita dos dados em arquivo TXT
    O comando get não está funcionando

--- Python Files/Teste com Tkinter.py
+++ Python Files/Teste com Tkinter.py
@@ -1,17 +1,42 @@
+import dataclasses
 from tkinter import *
+import os
+#from webbrowser import *
+#from requests import *
 
 app = Tk()
 app.title("APP 5G")
 app.geometry("500x500")
 app.configure(background="#118")
 
+
+
 txt1 = Label(app, text = "Bem-vindo a interface", background="#ff0", foreground= "#000")
-txt1.place(x=10,y=10,width=150, height=30)
+#txt1.grid(row= 5, column= 5) # Colocando o objeto por linhas e colunas.
+txt1.place(x=10,y=10,width=150, height=30) #Colocando o objeto por coodernadas
+
+
+c = os.path.dirname(__file__)
+nomeArquivo=c+"Dados.txt"
+
+
 
 def sair():
     quit()
 
 
+def impdados():
+    arquivo=open(nomeArquivo,"a")
+    arquivo.write("\nFrequência:    %s" %get(vf))
+    arquivo.write("\nPotência:    %s" %get(vp))
+    arquivo.write("\nLargura de banda:    %s" %get(vlb))
+    arquivo.write("\n\n")
+    arquivo.close()
+    
+    print("Frequência:    %s" %getopt(vf))
+    print("Potência:    %s" %get(vp))
+    print("Largura de banda:    %s" %get(vlb))
+   
 """
 vtxt="Opções"
 vbg="#ff0"
@@ -30,6 +55,7 @@ Label(app, text="Digite a Largura de Banda (em Hz):",background="#ff0",foregroun
 vlb= Entry(app).place(x=10,y=175,width=110,height=20)
 
 
+Button(app, text= "Imprimir", command= impdados).place(x =10, y =200, width=110, height=20)
 Button(app, text= "Sair",  command= sair).place(x=10,y=235,width=110,height=20)
 
 
