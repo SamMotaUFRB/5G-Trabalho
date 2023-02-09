1250540d0ff52994e87fadc7e3c23196561b752f
SamMotaUFRB <samuelmota@aluno.ufrb.edu.br>
2023-02-09 08:25:48 -0300

    Botão sair
    Foi adicionado o Botão sair

--- Python Files/Teste com Tkinter.py
+++ Python Files/Teste com Tkinter.py
@@ -8,6 +8,10 @@ app.configure(background="#118")
 txt1 = Label(app, text = "Bem-vindo a interface", background="#ff0", foreground= "#000")
 txt1.place(x=10,y=10,width=150, height=30)
 
+def sair():
+    quit()
+
+
 """
 vtxt="Opções"
 vbg="#ff0"
@@ -19,12 +23,14 @@ txt2.pack(ipadx=10,ipady=10,padx=90,pady=90,side="top",fill=X,expand=True)
 Label(app, text="Digite a Frequência (em Hz):",background="#ff0",foreground= "#000", anchor= W).place(x=10,y=50,width=160,height=20)
 vf = Entry(app).place(x=10,y=75,width=110,height=20)
 
-Label(app, text="Digite a Potência (em Watts):",background="#ff0",foreground= "#000", anchor= W).place(x=10,y=100,width=170,height=20)
+Label(app, text="Digite a Potência (em Watts):",background="#ff0",foreground= "#000", anchor= W).place(x=10,y=100,width=160,height=20)
 vp = Entry(app).place(x=10,y=125,width=110,height=20)
 
-Label(app, text="Digite a Largura de Banda (em Hz):",background="#ff0",foreground= "#000", anchor= W).place(x=10,y=150,width=180,height=20)
+Label(app, text="Digite a Largura de Banda (em Hz):",background="#ff0",foreground= "#000", anchor= W).place(x=10,y=150,width=190,height=20)
 vlb= Entry(app).place(x=10,y=175,width=110,height=20)
 
+
+Button(app, text= "Sair",  command= sair).place(x=10,y=235,width=110,height=20)
 
 
 
